8bce40453375ebbd2317e99f1a903988c3c30e4a
Wang Feng <maplerme@gmail.com>
2017-07-19 01:04:20 +0900

    Fix pwm gpio

--- farmy/modules/pump.py
+++ farmy/modules/pump.py
@@ -2,14 +2,15 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
 PIN_PUMP = 24
-GPIO.setup(PIN_PUMP, GPIO.OUT, initial=GPIO.LOW)
 
 
 def run_pwm(pin, power, seconds):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pin, GPIO.OUT, initial=GPIO.LOW)
     obj = GPIO.PWM(pin, 100)
     obj.start(0)
     obj.ChangeDutyCycle(power)
     time.sleep(seconds)
     obj.stop()
+    GPIO.cleanup()
