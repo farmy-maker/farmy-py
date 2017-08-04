a07a4c1212eeb59d94c7db13b9f20cb1936448f2
Wang Feng <maplerme@gmail.com>
2017-08-04 21:11:30 +0900

    Fix smbus lib

--- farmy/modules/light.py
+++ farmy/modules/light.py
@@ -1,5 +1,5 @@
 ﻿#!/usr/bin/python
-import smbus
+from smbus2 import SMBus
 import time
 
 DEVICE = 0x23
@@ -9,7 +9,7 @@ RESET = 0x07
 
 ONE_TIME_HIGH_RES_MODE_1 = 0x20
 
-bus = smbus.SMBus(1)
+bus = SMBus(1)
 
 
 def convert_to_number(data):
@@ -17,7 +17,7 @@ def convert_to_number(data):
 
 
 def read_light(addr=DEVICE):
-    data = bus.read_i2c_block_data(addr, ONE_TIME_HIGH_RES_MODE_1)
+    data = bus.read_i2c_block_data(addr, ONE_TIME_HIGH_RES_MODE_1, 16)
     return convert_to_number(data)
 
 
